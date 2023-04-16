1ff7f343b6a1469010d3968b7e37f08d411bc2b1
Maryna Medynska <50908486+aquamarinewave@users.noreply.github.com>
2023-04-16 20:22:27 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,33 +23,3 @@ const App = () => {
 };
 
 export default App;
-
-
- // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(window.localStorage.getItem('contacts')) ?? []
-  // });
-  // const [filter, setFilter] = useState('');
-
-  // const formSubmitHandler = newContact => {
-  //   const normName = newContact.name.toLowerCase();
-  //   contacts.find(({ name }) => name.toLowerCase() === normName)
-  //     ? alert(newContact.name + "is already in contscts")
-  //     : setContacts([...contacts, newContact]);
-  // };
-
-  // const deleteRecipe = contactId => {
-  //   setContacts(contacts.filter(contact => contact.id !== contactId));
-  // };
-
-  // const changeFilter = (event) => {
-  //   setFilter(event.currentTarget.value);
-  // }
-  
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter))
